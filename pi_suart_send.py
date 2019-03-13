609a8bc28d726bdd0d1fef4417d01807fb3689d1
Crazy <you@example.com>
2019-03-13 17:46:58 +0800

    串口通信 最终版 JSON协议 python

--- pi_suart_send.py
+++ pi_suart_send.py
@@ -24,21 +24,14 @@ def find_string(s,t):
         return True
     except(ValueError):
         return False 
-
-
 def send_pose_point(PassPose,targetPos):
        print ',PassPose',len(PassPose)
        set_pose = rospy.Publisher('set_robot_pose', PointCloud, queue_size=10)
        my_awesome_pointcloud = PointCloud()
        print ',PassPose',len(PassPose)
        for i in range(0,len(PassPose)):
-           print 'i = ',i
            my_awesome_pointcloud.points.append(Point32(PassPose[i]["x"], PassPose[i]["y"], 0.0))
        my_awesome_pointcloud.points.append(Point32(targetPos["x"], targetPos["y"], 0.0))
-       x =56.0
-       #my_awesome_pointcloud.points.append(Point32(x, 1.0, 0.0))
-       # my_awesome_pointcloud.points.append(Point32(2.0, 2.0, 0.0))
-       #my_awesome_pointcloud.points.append(Point32(3.0, 3.0, 0.0))
        set_pose.publish(my_awesome_pointcloud)
 def write_json(write_date):
      Fb = 'fb'.decode('hex')
@@ -51,18 +44,14 @@ def Analysis_json(recv):
      info = json.loads(recv)
      correct_date ={ "error_code":0} 
      write_json(correct_date)
-     #correct_date1 = json.dumps(correct_date)
-     #print 'json',correct_date1
-     #ser.write(correct_date1)
      print 'fianl_info',info
-     # print('passPos = ',info["content"]["passPos"][1]["x"])
      print('passPos = ',len(info["content"]["passPos"]))
      print('targetPos = ',info["content"]["targetPos"]["x"])
      send_pose_point(info["content"]["passPos"],info["content"]["targetPos"])
    except:
      error_date ={ "error_code":-1,"error_msg":"data lost Please reset"} 
      write_json(error_date)
-     print 'fail KKKKKKKKKKKKKKKKKKKKKKKKKKKKK'
+     print 'json fail'
 
 def Exclude_FA(bytes):
     FA = 'fa'.decode('hex')
@@ -71,57 +60,15 @@ def Exclude_FA(bytes):
         print i
         bytes=bytes.translate(None,FA)
     Analysis_json(bytes) 
-
 replce_str = ''
-n = 'aaa'
-
-def print_hex1(bytes):
-    FA = 'fa'.decode('hex')
-    global replce_str
-    
-    global n
-    print "KKKKKKKKKKKKKKKKKKKK",n
-    n = n+'b'
-    if find_string(bytes,FA) == False :
-        print 'date inter'
-        replce_str = ''
-        replce_str = bytes
-    else:
-         print 'copy1', replce_str
-         print 'OOOOOKKKK'
-         replce_str =replce_str+bytes
-         print 'copy', replce_str
-         print 'nihao '
-         replce_str1 = replce_str
-         replce_str = ''
-         Exclude_FA(replce_str1)
-         replce_str = ''
-         print 'over'
-    for i in bytes:
-     if i.encode('hex') == 'fa':
-        print i
-        bytes=bytes.translate(None,FA)
-        print 'OOOOOOLLLLLLLLLL'
-    l = [i.encode('hex') for i in bytes]
-    print(" ".join(l))
-
-
 def print_hex(bytes):
     FA = 'fa'.decode('hex')
     global replce_str
-    global n
-    print "KKKKKKKKKKKKKKKKKKKK",n
-    n = n+'b'
     if find_string(bytes,FA) == False :
         print 'date inter'
-        # replce_str = ''
         replce_str =replce_str+ bytes
     else:
-         #print 'replce_str', replce_str
-         #print 'OOOOOKKKK'
          replce_str =replce_str+bytes
-         #print 'copy', replce_str
-         #print 'nihao '
          print replce_str.find(FA)
          replce_str1 = replce_str[0:replce_str.find(FA)]
          replce_str = ''
@@ -151,7 +98,6 @@ def PoseCallBack(Twist):
     robot_date ={"content":{"position":{"x":Twist.angular.x,"y":Twist.angular.y},"speed":Twist.angular.z},"obstacle":Twist.linear.x,"command":"upload_status"}
     write_robot_json(robot_date)
     print ("send value is %x",(Twist.linear.x))
-    #ser.write((str(int(Twist.linear.x))))
 
 def main():
      signal.signal(signal.SIGINT, quit)                                
@@ -160,23 +106,10 @@ def main():
      rospy.Subscriber('/state_val',Twist,PoseCallBack)
      set_pose = rospy.Publisher('set_robot_pose', PointCloud, queue_size=10)
      #rospy.spin()
-     flag = 1
-     while flag == 1:
+     while 1:
        Deal_with_usart()
-       #data1 ="[ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 }] "
-       #data1 ={ "content": {"targetPos": {"x": 1.2,"y": 5.6},"passPos":[{ "x": 1.21,"y": 5.6},{"x": 1.22,"y": 5.6},{"x": 1.23,"y": 5.6}]},"command":"set_pos"} 
-
-       #json1 = json.dumps(data1)
-       #print 'json',json1
-       #ser.write(json1)
        my_awesome_pointcloud = PointCloud()
-       x =5.0
-       my_awesome_pointcloud.points.append(Point32(x, 1.0, 0.0))
-       my_awesome_pointcloud.points.append(Point32(2.0, 2.0, 0.0))
-       my_awesome_pointcloud.points.append(Point32(3.0, 3.0, 0.0))
-       #set_pose.publish(my_awesome_pointcloud)
        # rospy.spin()
-       # send_pose_point()
        print 'OOOKKK'
        time.sleep(0.5)
 if __name__ == '__main__':
@@ -184,5 +117,4 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         if ser != None:
-
             ser.close()
