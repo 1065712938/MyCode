6576d6901876540c5d076f74477b7e7774454a97
Crazy <you@example.com>
2019-03-29 10:46:11 +0800

    基于上位机控制 实现多点运行 无避障效果

--- mini_catkin_ws/src/mini_robot_control/src/mini_pid.cpp
+++ mini_catkin_ws/src/mini_robot_control/src/mini_pid.cpp
@@ -1578,23 +1578,25 @@ void linear_back_Random(float _Set_Point_X)
     Send_stop();
     float back_speed = 0;
    // _Set_Point_X = 10;取消上位机控制
-    // if(_Set_Point_X>4)
-    //  back_speed = 0.4;
-    // else back_speed = 0.2;
-    // amcl_linear_back_Random(0.07,back_speed,_Set_Point_X*4/5,1);
-    // amcl_linear_back_Random(back_speed,back_speed,_Set_Point_X/5,1);
-    // amcl_linear_back_Random(back_speed,0.07,-0.06,1);
+    if(_Set_Point_X>4) ////增加上位机控制
+     back_speed = 0.4;
+    else back_speed = 0.2;
+    amcl_linear_back_Random(0.07,back_speed,_Set_Point_X*4/5,1);
+    amcl_linear_back_Random(back_speed,back_speed,_Set_Point_X/5,1);
+    amcl_linear_back_Random(back_speed,0.07,-0.06,1);
+    /*
     back_speed = 0.2;
     amcl_linear_back_Random_Avoidance02(0.2,back_speed,_Set_Point_X*4/5,1);
     amcl_linear_back_Random_Avoidance02(back_speed,back_speed,_Set_Point_X/5,1);
     amcl_linear_back_Random_Avoidance02(back_speed,0.13,-0.06,1);
+    */
     Send_stop();
 }
 
 void Random_Pose_run()
 {
   ros::Rate loop_rate(10);
-//  while((g_Set_Points.points.size()==0)&&(ros::ok()))
+  while((g_Set_Points.points.size()==0)&&(ros::ok()))//增加上位机控制
   {
     cout<<"Set_Point_X = "<<Set_Point_X<<endl;
     cout<<"等待上位机发送指令"<<endl;
@@ -1603,13 +1605,13 @@ void Random_Pose_run()
   }
   float Set_X = 0;
   float Set_Y = 0;
- // for(unsigned int i = 0; i < g_Set_Points.points.size(); i++)
+  for(unsigned int i = 0; i < g_Set_Points.points.size(); i++)//增加上位机控制
   {
         //cout<<"目标坐标 1"<<g_Set_Points.points[i]<<endl;
-        //Set_X  = g_Set_Points.points[i].x;//Set_Point_X
-        //Set_Y = g_Set_Points.points[i].y;//Set_Point_Y
-        Set_X  = 6;//Set_Point_X
-        Set_Y  = 0;//Set_Point_Y
+        Set_X  = g_Set_Points.points[i].x;//Set_Point_X //增加上位机控制
+        Set_Y = g_Set_Points.points[i].y;//Set_Point_Y
+        // Set_X  = 6;// 取消上位机控制
+        // Set_Y  = 0;//
         float line_distance = 0;
         if(Set_Y==0)
         {
@@ -1625,8 +1627,8 @@ void Random_Pose_run()
           Arc_flag = 1;
           line_distance = Set_X - 2.0;
         }     
-        // linear_ahead_random(0.07,0.4,line_distance,0,Increase_speed,0);
-        linear_ahead_random(0.2,0.2,line_distance,0,Increase_speed,0);        
+         linear_ahead_random(0.07,0.4,line_distance,0,Increase_speed,0);//增加上位机控制
+        //linear_ahead_random(0.2,0.2,line_distance,0,Increase_speed,0);        
         if(Arc_flag == 0)
         {
             Send_stop();
